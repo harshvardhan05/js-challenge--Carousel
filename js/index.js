37218104b5cce0283259f60f25e148b8a7ac979b
Harshvardhan <hv050694@gmail.com>
2021-06-12 21:13:26 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const btnPrev = document.querySelector("#carousel-button-previous")
+const btnNext = document.querySelector("#carousel-button-next") 
+
+let carousel = document.querySelectorAll(".carousel-item")
+console.log(carousel)
